2cc12784cf6fdffc6d8f61428b49d75e46eee401
Brian Wandell <wandell@stanford.edu>
2016-10-23 14:50:50 -0700

    How to read the data updated Need to ask LMP questions about which file. Almost ready for implementing the model calculation based on S0 exp(-b vQv') prediction.

--- src/dtiError.m
+++ src/dtiError.m
@@ -11,9 +11,11 @@ function err = dtiError(dtiInitZip,parametersJSON)
 %   addpath(genpath(pwd))
 %   chdir(fullfile(dtiErrorRootPath,'local'))
 %   
+
+%% For getting started, we go get an example from Flywheel
+
 st = scitran('action', 'create', 'instance', 'scitran');
-% 
-%
+
 %% List all projects
 
 % In this example, we use the structure 'srch' to store the search parameters.
@@ -36,24 +38,69 @@ for ii=1:length(sessions)
     end
 end
 
-%% Another way to search, but it takes a long time
+%% This takes a long time, but it is one of this thisZip results
+
+% So we know we can find it.
 fname = 'dtiInit_03-Oct-2016_21-17-04.zip';
+[~,baseName,~] = fileparts(fname);
+baseDir = fullfile(dtiErrorRootPath,'local');
+
 clear srch
 srch.path = 'analyses/files';
 srch.files.match.name = fname;
 files = st.search(srch);
-dtiOutput = fullfile(dtiErrorRootPath,'local','dtiOutput.zip');
+dtiOutput = fullfile(baseDir,'dtiOutput.zip');
 st.get(files{1},'destination',dtiOutput);
 unzip(dtiOutput);
 
-%%
-[p,n,e] = fileparts(fname);
-baseDir = fullfile(dtiErrorRootPath,'local',n);
-bvecsFile = mrvFindFile('*.bvecs',baseDir)
-bvecs = textread(bvecsFile);
-bvalsFile = mrvFindFile('*.bvals',baseDir);
-bvals = textread(bvalsFile);
+% New base directory for the DWI data
+baseDir = fullfile(baseDir,baseName);
 
+%% Identify and load the dwi and metadata files
+
+% Ask LMP:  It seems to me that the 'aligned' files in this base directory
+% are aligned to the T1.  
+% Not sure if these in the base directory are the ones we want, or the
+% trilin directory ones.
+clear dwi
+d = dir(fullfile(baseDir,'*aligned*.nii.gz'));
+baseName = fullfile(baseDir,d.name);
+exist(baseName,'file')
+dwi = dwiLoad(baseName);
+dwiPlot(dwi,'bvecs');
+
+%% Read the b=0 file  
+
+% Ask LMP:  We should probably be getting the b0 from the bin directory
+% inside of dti31trilin.  But this is the idea.
+d = dir(fullfile(baseDir,'*b0*.nii.gz'));
+b0Name = fullfile(baseDir,d.name);
+exist(b0Name,'file')
+
+b0 = niftiRead(b0Name);
+niftiView(b0);
+
+%% Read the tensors
+
+% Why are the directories like this: dti<N>trilin?
+% Can't we give the directory a reliable name?  We can always figure out N
+% from the bvecs file.
+
+% The predicted DWI data from the
+
+
+%%  Other ways I have read and found files
+
+% [p,n,e] = fileparts(fname);
+% baseDir = fullfile(dtiErrorRootPath,'local',n);
+% bvecsFile = dir('*.bvecs'); % mrvFindFile('*.bvecs',baseDir);
+% fname = bvecsFile.name; exist(fname,'file'); bvecs = dlmread(fname);
+% bvalsFile = dir('*.bvals'); % mrvFindFile('*.bvecs',baseDir);
+% fname = bvalsFile.name; exist(fname,'file'); bvals = dlmread(fname);
+
+% mrvFindFile('*.bvals',baseDir)
+
+%%
 tensorsFile = mrvFindFile('tensors.nii.gz',baseDir);
 dt6Data = dtiLoadTensorsFromNifti(tensorsFile);
 % coords = [40:44;40:44;40:44]';
